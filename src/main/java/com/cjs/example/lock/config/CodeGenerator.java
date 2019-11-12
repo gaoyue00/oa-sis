ce02795c8a20b6780668aeec3672a92175fd52ef
gaoyue <yue.gao@oasismedical.cn>
2019-11-12 09:41:26 +0800

    添加逆向工程

--- src/main/java/com/cjs/example/lock/config/CodeGenerator.java
+++ src/main/java/com/cjs/example/lock/config/CodeGenerator.java
@@ -1,37 +1,29 @@
 package com.cjs.example.lock.config;
 
-import java.util.*;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
-import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
+import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
-import com.baomidou.mybatisplus.generator.config.rules.FileType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
-import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
 
 /**
- * @描述：
- * @author： yue.gao
- * @date： 2019/11/11 19:07
+ * @ProjectName: mybatis-plus
+ * @Package: com.jiangfeixiang.mybatisplus.mpconfig
+ * @ClassName: CodeGenerator
+ * @Description: 代码生成器
  */
-public class Generator {
-
-
-
+public class CodeGenerator {
 
     /**
-     * <p>
      * 读取控制台内容
-     * </p>
      */
     public static String scanner(String tip) {
         Scanner scanner = new Scanner(System.in);
@@ -48,38 +40,54 @@ public class Generator {
     }
 
     public static void main(String[] args) {
-        // 代码生成器
+        /**
+         * 代码生成器
+         */
         AutoGenerator mpg = new AutoGenerator();
 
-        // 全局配置
-        GlobalConfig gc = new GlobalConfig();
+        /**
+         * 全局配置
+         */
+        GlobalConfig globalConfig = new GlobalConfig();
+        //生成文件的输出目录
         String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir(projectPath + "/src/main/java");
-
+        globalConfig.setOutputDir(projectPath + "/src/main/java");
+        //Author设置作者
+        globalConfig.setAuthor("gy");
         //是否覆盖文件
-        gc.setFileOverride(true);
-        gc.setAuthor("gaoyue");
-        gc.setOpen(false);
-        // gc.setSwagger2(true); 实体属性 Swagger2 注解
-        mpg.setGlobalConfig(gc);
+        globalConfig.setFileOverride(true);
+        //生成后打开文件
+        globalConfig.setOpen(false);
+        mpg.setGlobalConfig(globalConfig);
 
-        // 数据源配置
-        DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setDbType(DbType.MYSQL);
-        dsc.setUrl(/*PrimaryDataBaseConfig.url*/"jdbc:mysql://60.205.106.190:3306/test_gy?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&autoReconnect=true&useSSL=false");
+        /**
+         * 数据源配置
+         */
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        // 数据库类型,默认MYSQL
+        dataSourceConfig.setDbType(DbType.MYSQL);
+        //自定义数据类型转换
+        dataSourceConfig.setTypeConvert(new MySqlTypeConvert());
+        dataSourceConfig.setUrl(/*PrimaryDataBaseConfig.url*/"jdbc:mysql://60.205.106.190:3306/test_gy?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&autoReconnect=true&useSSL=false");
         // dsc.setSchemaName("public");
-        dsc.setDriverName("com.mysql.jdbc.Driver");
-        dsc.setUsername(/*PrimaryDataBaseConfig.username*/"root");
-        dsc.setPassword(/*PrimaryDataBaseConfig.password*/"oasisadmin");
-        mpg.setDataSource(dsc);
+        dataSourceConfig.setDriverName("com.mysql.jdbc.Driver");
+        dataSourceConfig.setUsername(/*PrimaryDataBaseConfig.username*/"root");
+        dataSourceConfig.setPassword(/*PrimaryDataBaseConfig.password*/"oasisadmin");
+        mpg.setDataSource(dataSourceConfig);
 
-        // 包配置
+        /**
+         * 包配置
+         */
         PackageConfig pc = new PackageConfig();
-//        pc.setModuleName(scanner("模块名"));
+        pc.setModuleName(scanner("模块名"));
+        //父包名。如果为空，将下面子包名必须写全部， 否则就只需写子包名
         pc.setParent("com.cjs.example.lock");
         mpg.setPackageInfo(pc);
 
-        // 自定义配置
+
+        /**
+         * 自定义配置
+         */
         InjectionConfig cfg = new InjectionConfig() {
             @Override
             public void initMap() {
@@ -87,36 +95,34 @@ public class Generator {
             }
         };
 
-        // 如果模板引擎是 freemarker
+        /**
+         * 模板
+         */
+        //如果模板引擎是 freemarker
         String templatePath = "/templates/mapper.xml.ftl";
         // 如果模板引擎是 velocity
         // String templatePath = "/templates/mapper.xml.vm";
 
-        // 自定义输出配置
+
+        /**
+         * 自定义输出配置
+         */
         List<FileOutConfig> focList = new ArrayList<>();
         // 自定义配置会被优先输出
         focList.add(new FileOutConfig(templatePath) {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return projectPath + "/src/main/resources/mappers/" + pc.getModuleName()
+                return projectPath + "/src/main/resources/mapper/"+ pc.getModuleName()
                         + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
-
-//        cfg.setFileCreate(new IFileCreate() {
-//            @Override
-//            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
-//                // 判断自定义文件夹是否需要创建
-//                checkDir("调用默认方法创建的目录");
-//                return false;
-//            }
-//        });
-
         cfg.setFileOutConfigList(focList);
         mpg.setCfg(cfg);
 
-        // 配置模板
+        /**
+         * 配置模板
+         */
         TemplateConfig templateConfig = new TemplateConfig();
 
         // 配置自定义输出模板
@@ -128,25 +134,28 @@ public class Generator {
         templateConfig.setXml(null);
         mpg.setTemplate(templateConfig);
 
-        // 策略配置
+        /**
+         * 策略配置
+         */
         StrategyConfig strategy = new StrategyConfig();
-
+        //设置命名格式
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-        strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
+        strategy.setInclude(scanner("表名,多个英文逗号分割").split(","));
         //实体是否为lombok模型（默认 false）
         strategy.setEntityLombokModel(true);
+        //生成 @RestController 控制器
         strategy.setRestControllerStyle(true);
-        // 公共父类
-        strategy.setSuperControllerClass("com.baomidou.ant.common." +
-                "");
-        // 写于父类中的公共字段
-//        strategy.setSuperEntityColumns("id");
-        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        //设置自定义继承的Entity类全称，带包名
+        //strategy.setSuperEntityClass("com.jiangfeixiang.mpdemo.BaseEntity");
+        //设置自定义继承的Controller类全称，带包名
+        //strategy.setSuperControllerClass("com.jiangfeixiang.mpdemo.BaseController");
+        //设置自定义基础的Entity类，公共字段
+        strategy.setSuperEntityColumns("id");
         //驼峰转连字符
         strategy.setControllerMappingHyphenStyle(true);
+        //表名前缀
         strategy.setTablePrefix(pc.getModuleName() + "_");
-
         mpg.setStrategy(strategy);
         mpg.setTemplateEngine(new FreemarkerTemplateEngine());
         mpg.execute();
